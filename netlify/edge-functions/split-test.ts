b941f1a6639db39d6c597f75fb839bf54d0164d7
Mikael Andersen <mikael.svenstrup.andersen@sallinggroup.com>
2022-10-31 12:43:52 +0100

    New deploy url

--- netlify/edge-functions/split-test.ts
+++ netlify/edge-functions/split-test.ts
@@ -1,7 +1,7 @@
 export default async (request: Request, context: any) => {
   //const buckets = JSON.parse(Deno.env.get("AB_TEST_LIST") || "null");
 
-  const buckets = [{ url: "https://edge-handler-poc.netlify.app", weight: 0.5 }, { url: "https://deploy-preview-1--edge-handler-poc.netlify.app", weight: 0.5 }]
+  const buckets = [{ url: "https://edge-handler-poc.netlify.app", weight: 0.5 }, { url: "https://deploy-preview-4--edge-handler-poc.netlify.app", weight: 0.5 }]
   //If environment variable not set return standard page
   if (!buckets || !request) {
     return context.next();
